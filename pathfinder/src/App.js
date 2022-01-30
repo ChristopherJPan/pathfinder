4c6a7724c2f3d99366880808fea17dec7afb487b
Relai1 <christopherjpan@gmail.com>
2022-01-29 20:15:33 -0800

    added drop down

--- pathfinder/src/App.js
+++ pathfinder/src/App.js
@@ -1,14 +1,36 @@
 import React, { useState } from "react";
 import "./App.css";
+import Dropdown from "./Dropdown.js"
+
+  const items = [
+    {
+      id: 1,
+      value: "Pulp Fiction",
+    },
+    {
+      id: 2,
+      value: "The Prestige",
+    },
+    {
+      id: 3,
+      value: "Blade Runner 2049",
+    },
+  ];
 
 function App() {
   const [hello, setHello] = useState("hello");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hi Chris</p>
-      </header>
+    <div className="container">
+      <h1 style={{ textAlign: 'center' }}>
+        Buy Movies{' '}
+        <span role="img" aria-label="Movie projector">
+          🎥
+        </span>
+      </h1>
+      <Dropdown title="Select movie" items={items} multiSelect />
     </div>
+
   );
 }
 
